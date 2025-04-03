72f85e2a27048cf4a375b441666711c55b1f215c
17Reyes <1707reyes@gmail.com>
2025-04-03 17:57:28 -0600

    Add files via upload

---
+++ PwaFinal/service-worker.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'mictlans-gate-cache-v3';
+
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
